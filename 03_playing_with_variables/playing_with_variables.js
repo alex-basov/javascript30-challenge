3ee839ea956379d8adaa3eab2329eb15610babab
Alex Basov <bolex.ua@gmail.com>
2018-12-23 22:29:36 +0200

    update image properties on input value change

--- 03_playing_with_variables/playing_with_variables.js
+++ 03_playing_with_variables/playing_with_variables.js
@@ -1,9 +1,12 @@
 const inputs = document.querySelectorAll('.controls input');
 
-function hadnleUpdate() {
-  console.log(this.value);
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
-inputs.forEach(input => input.addEventListener('change', hadnleUpdate));
-
-console.log(inputs);
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
